e64a63c4848d5a86227e330b38c00437eb233005
MuhamadRodian31 <147028805+MuhamadRodian31@users.noreply.github.com>
2023-10-12 14:26:59 +0700

    Add files via upload

---
+++ Tugas Pertemuan Praktikum Pemograman C2/T2- Alas Segitiga.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a= 8;
+    int t= 5;
+    int hasil= 0.5*a*t;
+
+    printf("Hasil dari luas segitga adalah %d ", hasil);
+
+    return 0;
+}
---
+++ Tugas Pertemuan Praktikum Pemograman C2/T2-Sisi Miring.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int alas= 4;
+    int tinggi = 5;
+    int hasil;
+
+    hasil= (alas*alas) + (tinggi*tinggi);
+
+    printf("Hasil sisi miring segitiga adlaah %d", hasil);
+
+    return 0;
+}
