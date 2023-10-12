4005e15d05b013ed05fdb48f817411dde6b98420
MuhamadRodian31 <147028805+MuhamadRodian31@users.noreply.github.com>
2023-10-12 14:25:52 +0700

    Add files via upload

---
+++ Tugas Pertemuan Praktikum Pemograman C1/T1-NIM.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int nim = 2143012;
+    char* nama = "Muhamad Rodian Ihban";
+    char* jurusan = "if";
+
+    printf("nim  : %d \n", nim);
+    printf("nama : %s \n", nama);
+    printf("jurusan : %s \n", jurusan);
+
+    return 0;
+}
---
+++ Tugas Pertemuan Praktikum Pemograman C1/T2-Persegi Panjang.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int p = 10;
+    int l = 7;
+    int h = p*l;
+
+    printf("hasil : %d", h);
+    return 0;
+}
